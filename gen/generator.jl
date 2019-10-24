68f9cf442177c558d331f910a605de97174d2ff8
WooKyoung Noh <wookay.noh@gmail.com>
2019-10-24 12:10:24 +0900

    fix gen/generator.jl

--- gen/generator.jl
+++ gen/generator.jl
@@ -1,7 +1,14 @@
+using BinaryProvider
+
+tarball_url = "https://dd.b.pvp.net/datadragon-set1-lite-en_us.zip"
+hash = "3f3e174d49597f1e6c5136391b7be28a42dfda9df3842182b210a9a2de85389b" # using SHA; bytes2hex(sha256(read(file)))
+prefix = Prefix(normpath(@__DIR__, "datadragon-set1-lite-en_us"))
+!isinstalled(tarball_url, hash, prefix=prefix) && install(tarball_url, hash, prefix=prefix)
+
 using JSON2
 
 bundle_set = normpath(@__DIR__, "bundle_set.jl")
-set1 = normpath(@__DIR__, "../deps/datadragon-set1-lite-en_us/en_us/data/set1-en_us.json")
+set1 = normpath(@__DIR__, "datadragon-set1-lite-en_us/en_us/data/set1-en_us.json")
 
 include(normpath(@__DIR__, "../src/types.jl"))
 function Card(nt::NamedTuple)
